b0912e19349d1b427094ab7258e193801168586d
Henil5204 <101511850@georgebrown.ca>
2025-03-11 10:48:24 -0400

    added

--- week10/main.py
+++ week10/main.py
@@ -3,3 +3,22 @@ from contextlib import closing
 
 
 db_path="sqlite.db"
+
+try:
+    with closing(sqlite3.connect(db_path)) as db_con:
+        db_con.row_factory = sqlite3.Row
+        with closing(db_con.cursor()) as cursor:
+        try:
+            query_1 = "SELECT * FROM demo where id > 14:"
+            cursor.execute(query_1)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row["name"])
+        except Exception as e:
+            print(f"Error excuting query_1: {e}")
+        try:
+            del_row=input("Enter the row ID threshold for deletion:")
+            
+        except:    
+except ValueError as e:
+    print("--")
